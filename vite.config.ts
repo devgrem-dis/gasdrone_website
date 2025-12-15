d3ee2db2af912597334ce550ecf61e51140d073d
devgrem-dis <dev@gremtech.cl>
2025-12-14 22:39:11 -0300

    h
    j

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gasdrone_website/", // <- reemplaza con el nombre de tu repo en GitHub
   server: {
     host: "::",
     port: 8080,
